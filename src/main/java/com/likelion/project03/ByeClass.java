618810585202cd14f824df3c36314d08159ded2f
선녀♥\sn <missksn@naver.com>
2023-04-19 13:10:17 +0900

    feat: Bye 기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/likelion/project03/ByeClass.java
@@ -0,0 +1,7 @@
+package com.likelion.project03;
+
+public class ByeClass {
+    public void print() {
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/likelion/project03/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.likelion.project03;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass ByeClass = new ByeClass(); // 인스턴스화
+        ByeClass.print();
+    }
+}
