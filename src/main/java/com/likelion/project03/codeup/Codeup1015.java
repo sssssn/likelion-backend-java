8bce6cea11f17d0577f96fa0f70537e7c5197599
선녀♥\sn <missksn@naver.com>
2023-04-20 16:46:46 +0900

    feat: Codeup 문제풀이 - Codeup1015 추가

---
+++ src/main/java/com/likelion/project03/codeup/Codeup1015.java
@@ -0,0 +1,10 @@
+package com.likelion.project03.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.2f", sc.nextFloat());
+    }
+}
