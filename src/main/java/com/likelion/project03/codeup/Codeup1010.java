4d3c322fd2376f9965137a7d1a2b300b52be541d
선녀♥\sn <missksn@naver.com>
2023-04-20 11:41:52 +0900

    feat: Codeup 문제 풀이 - Codeup1010 추가

---
+++ src/main/java/com/likelion/project03/codeup/Codeup1010.java
@@ -0,0 +1,11 @@
+package com.likelion.project03.codeup;
+
+import java.util.Scanner;
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+
+}
