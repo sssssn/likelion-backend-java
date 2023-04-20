e8600b9ed7c5ba03df621f9b0d866e34737cbdfc
선녀♥\sn <missksn@naver.com>
2023-04-20 09:31:08 +0900

    feat: 숫자 2개 입력받아 더하기 복습 - ScannerEx2, ScannerExTest2 추가

---
+++ src/main/java/com/likelion/project03/week1/ScannerEx2.java
@@ -0,0 +1,11 @@
+package com.likelion.project03.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx2 {
+    public void inputTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+
+    }
+}
---
+++ src/main/java/com/likelion/project03/week1/ScannerExTest2.java
@@ -0,0 +1,8 @@
+package com.likelion.project03.week1;
+
+public class ScannerExTest2 {
+    public static void main(String[] args) {
+        ScannerEx2 scannerEx2 = new ScannerEx2();
+        scannerEx2.inputTwoNumbersAndPlus();
+    }
+}
