db9a828ef382f834580463dbe69329ca01f088a5
sn <missksn@naver.com>
2023-04-21 13:32:27 +0900

    fix: 인코딩 설정 변경 - 한글 주석 깨짐 발견하여 로컬 UTF-8로 변경

--- src/main/java/com/likelion/project03/codeup/Codeup1017.java
+++ src/main/java/com/likelion/project03/codeup/Codeup1017.java
@@ -6,7 +6,7 @@ public class Codeup1017 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-//        System.out.println(num + " " + num + " " +num); // 规过1
-        System.out.printf("%d %d %d", num, num, num); // 规过2
+//        System.out.println(num + " " + num + " " +num); // 氚╇矔1
+        System.out.printf("%d %d %d", num, num, num); // 氚╇矔2
     }
 }
