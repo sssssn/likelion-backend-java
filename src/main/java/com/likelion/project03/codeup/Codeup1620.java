51aa005428615c41a06e47d01dc11cf722d89133
sn <doitforyourself0603@gmail.com>
2023-05-03 09:05:59 +0900

    feat: 강의 내용 실습 및 Codeup 문제 풀이 - while문 등 강의 내용 실습 및 Codeup1083~1086, 1620 추가

---
+++ src/main/java/com/likelion/project03/codeup/Codeup1620.java
@@ -0,0 +1,30 @@
+package com.likelion.project03.codeup;
+
+/* 문제 1620
+어떤 수 n이 입력되면 n의 각 자릿수의 합이 한 자리가 될때까지 계산하여 출력하시오.
+
+예) 1234567
+
+1234567 → 1+2+3+4+5+6+7 = 28 → 2 + 8 = 10 → 1 + 0 = 1
+ */
+
+import java.util.Scanner;
+
+public class Codeup1620 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(digitSum(n));
+    }
+
+    public static int digitSum(long n) {
+        if (n < 10) return (int)n;
+
+        long sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return digitSum(sum);
+    }
+}
---
+++ src/main/java/com/likelion/project03/week3/day2/WhileCountDown.java
@@ -0,0 +1,10 @@
+package com.likelion.project03.week3.day2;
+
+public class WhileCountDown {
+    public static void main(String[] args) {
+        int cnt = 5;
+        while (cnt > 0) {
+            System.out.println(cnt--);
+        }
+    }
+}
