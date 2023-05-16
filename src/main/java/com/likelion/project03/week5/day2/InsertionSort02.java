e8d0b31e7e1a22e11c9f7f3f87c26467cf9fdb0d
sssssn <doitforyourself0603@gmail.com>
2023-05-16 19:45:14 +0900

    feat: 삽입, 선택 정렬 등 강의 내용 실습 - InsertionSort, SelectionSort 추가

---
+++ src/main/java/com/likelion/project03/week5/day2/InsertionSort02.java
@@ -0,0 +1,26 @@
+package com.likelion.project03.week5.day2;
+
+import java.util.Arrays;
+
+public class InsertionSort02 {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 3, 9, 28, 11};
+        // 1 1 0
+        // 2 2 1
+        // 2 1 2
+        // 3 3 2
+        // 3 2 1
+        // 3 1 0
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+//                System.out.printf("i:%d j:%d j-1:%d\n", i, j, j - 1);
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[i];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
