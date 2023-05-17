1a5a14b9378d6725007f0443f5e4e9862fda51ad
sssssn <doitforyourself0603@gmail.com>
2023-05-17 16:11:03 +0900

    feat: Stack 등 강의 내용 실습 - Stack1~4 추가

---
+++ src/main/java/com/likelion/project03/week5/day3/Stack4.java
@@ -0,0 +1,36 @@
+package com.likelion.project03.week5.day3;
+
+public class Stack4 {
+
+    private int[] arr = new int[10000];
+    private int pointer = 0;
+
+    public void push(int value) {
+        this.arr[pointer++] = value;
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println("pointer = " + pointer);;
+    }
+
+    public boolean isEmpty() {
+        return this.pointer == 0;
+    }
+    public int peek() {
+        /*
+        push(10);
+        0 1
+        10 0 pointer:1
+         */
+        if(isEmpty()) throw new RuntimeException("스택이 비었습니다.");
+        return this.arr[pointer - 1];
+    }
+
+    public static void main(String[] args) {
+        Stack4 st = new Stack4();
+        st.push(10);
+        System.out.println(st.peek());
+        System.out.println(st.peek());
+        System.out.println(st.peek());
+        st.push(20);
+        System.out.println(st.peek());
+    }
+}
