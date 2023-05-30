db0b1f00b45cd117c8964aafcfa7a15b677f917e
sssssn <doitforyourself0603@gmail.com>
2023-05-30 18:59:07 +0900

    feat: 강의 내용 실습 - 0부터 99까지 369게임(재귀함수)

--- src/test/java/com/likelion/project02/week7/day2/MainTest.java
+++ src/test/java/com/likelion/project02/week7/day2/MainTest.java
@@ -16,4 +16,12 @@ class MainTest {
         assertEquals(false, Main.isLeapYear(1900));
         assertEquals(true, Main.isLeapYear(2400));
     }
+
+    void testis369() {
+        assertEquals("*", Main.is369(3));
+        assertEquals("", Main.is369(15));
+        assertEquals("**", Main.is369(33));
+        assertEquals("***", Main.is369(333));
+        assertEquals("****", Main.is369(3369));
+    }
 }
