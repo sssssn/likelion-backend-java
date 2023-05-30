b56ec50897d411ef36e2ab34ce59d20c4e9c4aed
sssssn <doitforyourself0603@gmail.com>
2023-05-30 19:11:32 +0900

    feat: 강의 내용 실습 - 윤년구하기, 369게임 MyUtils 이동

--- src/test/java/com/likelion/project02/week7/day2/MainTest.java
+++ src/test/java/com/likelion/project02/week7/day2/MainTest.java
@@ -2,26 +2,26 @@ package com.likelion.project02.week7.day2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void checkLeapYear() {
-        assertEquals(false, Main.checkLeapYear(1700));
+        assertEquals(false, MyUtils.checkLeapYear(1700));
     }
 
     void testLeapYear() {
-        assertEquals(true, Main.isLeapYear(2020));
-        assertEquals(false, Main.isLeapYear(1900));
-        assertEquals(true, Main.isLeapYear(2400));
+        assertEquals(true, MyUtils.isLeapYear(2020));
+        assertEquals(false, MyUtils.isLeapYear(1900));
+        assertEquals(true, MyUtils.isLeapYear(2400));
     }
 
     void testis369() {
-        assertEquals("*", Main.is369(3));
-        assertEquals("", Main.is369(15));
-        assertEquals("**", Main.is369(33));
-        assertEquals("***", Main.is369(333));
-        assertEquals("****", Main.is369(3369));
+        assertEquals("*", MyUtils.is369(3));
+        assertEquals("", MyUtils.is369(15));
+        assertEquals("**", MyUtils.is369(33));
+        assertEquals("***", MyUtils.is369(333));
+        assertEquals("****", MyUtils.is369(3369));
     }
 }
