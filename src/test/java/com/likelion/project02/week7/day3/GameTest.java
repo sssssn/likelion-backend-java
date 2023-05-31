962890dd59ec960bfb880bf284f975f6e4dd571f
sssssn <doitforyourself0603@gmail.com>
2023-05-31 15:58:04 +0900

    feat: 강의 내용 실습 - 두 수의 합이 9 이하가 되도록 테스트 추가

--- src/test/java/com/likelion/project02/week7/day3/GameTest.java
+++ src/test/java/com/likelion/project02/week7/day3/GameTest.java
@@ -29,4 +29,16 @@ public class GameTest {
         assertEquals("한번 더 해보자", game.getCheeringUpMsg());
         assertEquals(true, game.isAnswer(2));
     }
+
+    @Test
+    public void testRandom() {
+        System.out.println( (int)(Math.random() * 10) );
+    }
+
+    @Test
+    public void testSumMax() {
+        int a = 0;
+        int b = 0;
+        assertTrue(a + b < 10);
+    }
 }
