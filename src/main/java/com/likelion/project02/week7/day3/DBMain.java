1d15b9792be1fd0e4a30e4ec3e260ca8f0fd7e0e
sssssn <doitforyourself0603@gmail.com>
2023-05-31 14:37:41 +0900

    feat: 강의 내용 실습 - person table 생성 및 기본값 입력

--- src/main/java/com/likelion/project02/week7/day3/DBMain.java
+++ src/main/java/com/likelion/project02/week7/day3/DBMain.java
@@ -4,7 +4,58 @@ import java.sql.*;
 
 public class DBMain {
     public static void main(String[] args) {
-        String sql = "select 1+1 as result";
+        initPerson();
+    }
+
+    private static void initPerson() {
+        Connection conn = null;
+        Statement smt = null;
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
+            smt = conn.createStatement();
+            smt.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate("create table person (id integer, name string)");
+            smt.executeUpdate("insert into person values(1, 'sn')");
+            smt.executeUpdate("insert into person values(2, 'tofu')");
+            rs = smt.executeQuery("select * from person");
+            while(rs.next()) {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            try {
+                if (rs != null) rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (smt != null) smt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (psmt != null) psmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void getCodeName() {
+        String sql = "select code, name from country order by code, name";
         Connection conn = null;
         PreparedStatement psmt = null;
         ResultSet rs = null;
@@ -13,7 +64,8 @@ public class DBMain {
             psmt = conn.prepareStatement(sql);
             rs = psmt.executeQuery();
             while( rs.next() ) {
-                System.out.println( rs.getString("result"));
+                System.out.print( rs.getString("code") + "\t");
+                System.out.println( rs.getString("name"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
