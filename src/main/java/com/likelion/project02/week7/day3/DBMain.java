78a0505637b46e9e97fe1db02315cb8531ce0c20
sssssn <doitforyourself0603@gmail.com>
2023-05-31 14:44:51 +0900

    feat: 강의 내용 실습 - person table에 id 자동 증가, 테이블 생성과 추가를 메서드로 분리

--- src/main/java/com/likelion/project02/week7/day3/DBMain.java
+++ src/main/java/com/likelion/project02/week7/day3/DBMain.java
@@ -6,24 +6,39 @@ public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
         dbMain.initPerson();
-        dbMain.getCodeName();
+        dbMain.insertPerson("sn");
+        dbMain.insertPerson("tofu");
+    }
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
     }
 
     private void initPerson() {
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
         try {
             getConn();
             smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
             smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'sn')");
-            smt.executeUpdate("insert into person values(2, 'tofu')");
-            rs = smt.executeQuery("select * from person");
-            while(rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
+            smt.executeUpdate(sql);
         }
         catch(SQLException e) {
             System.err.println(e.getMessage());
